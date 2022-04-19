0bf97ed6fb8d3bbaefe6680c2035f0be9e8e6c46
David Kyalo <davidkymutinda@gmail.com>
2022-04-19 12:59:37 -0700

    Update printf.c

--- printf.c
+++ printf.c
@@ -1,6 +1,22 @@
 #include "main.h"
 #include <stdarg.h>
+/**
+ * print_string - prints string
+ * @p: string
+ * Return: void
+ */
+void print_string(char *p)
+{
+	int n;
 
+	n = 0;
+
+	while (p[n] != '\0')
+	{
+		_putchar(p[n]);
+		n++;
+	}
+}
 /**
  * _printf - A function that prints inuts to the std output
  * @format: Format by which to print the characters passed to function
@@ -12,11 +28,40 @@
 int _printf(const char *format, ...)
 {
 	int i;
+	va_list ap;
+	char charc;
+	char *p;
 
+	va_start(ap, format);
 	i = 0;
 
 	while (format[i] != '\0')
 	{
+		if (format[i] == '%')
+		{
+			i++;
+			switch (format[i])
+			{
+				case 'c':
+					charc = va_arg(ap, int);
+					_putchar(charc);
+					break;
+
+				case 's':
+					p = va_arg(ap, char*);
+					print_string(p);
+					break;
+
+				case '%':
+					_putchar('%');
+					break;
+
+				default:
+					break;
+			}
+			i++;
+			continue;
+		}
 		_putchar(format[i]);
 		i++;
 	}
