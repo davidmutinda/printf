43bac5c42cb0fc6debea9633747199066a6803b9
Gideon-O <osorogideon10@gmail.com>
2022-04-19 11:52:37 -0700

    _putchar function

---
+++ _putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * _putchar - A function that prints charaacters
+ * @c: Character to be printed
+ *
+ * Return: Always 1
+ */
+
+int _putchar(char c)
+{
+	return (write(i, &c, 1));
+}
