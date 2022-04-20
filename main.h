79fcb665bf72f2441658b0d470e78d06187a03f5
David Kyalo <davidkymutinda@gmail.com>
2022-04-20 04:55:01 -0700

    Update main.h

--- main.h
+++ main.h
@@ -1,7 +1,7 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
-
+#include <stdarg.h>
 int _printf(const char *format, ...);
 int _putchar(char c);
-
+void switch_statement(const char *format, int i, int num, va_list ap);
 #endif
