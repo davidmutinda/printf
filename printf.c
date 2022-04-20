ffdac7180d4884da4dec0641dcd5d9aa6b15ccbb
David Kyalo <davidkymutinda@gmail.com>
2022-04-20 04:55:57 -0700

    Update printf.c

--- printf.c
+++ printf.c
@@ -4,22 +4,6 @@
 #include <stddef.h>
 #include <stdlib.h>
 /**
- * print_string - prints string
- * @p: string
- * Return: void
- */
-void print_string(char *p)
-{
-	int n;
-
-	n = 0;
-	while (p[n] != '\0')
-	{
-		_putchar(p[n]);
-		n++;
-	}
-}
-/**
  * _printf - A function that prints inuts to the std output
  * @format: Format by which to print the characters passed to function
  * @...: Multiple function parameters
@@ -31,8 +15,6 @@ int _printf(const char *format, ...)
 {
 	int i = 0, num = 0;
 	va_list ap;
-	char charc;
-	char *p;
 
 	if (format == NULL)
 		exit(0);
@@ -42,35 +24,10 @@ int _printf(const char *format, ...)
 		if (format[i] == '%')
 		{
 			i++;
-			switch (format[i])
-			{
-				case 'c':
-					charc = va_arg(ap, int);
-					num -= 1;
-					_putchar(charc);
-					break;
-				case 's':
-					p = va_arg(ap, char*);
-					if (p == NULL)
-						p = "(null)";
-					num = strlen(p) - 2;
-					print_string(p);
-					break;
-				case '%':
-					_putchar('%');
-					num -= 1;
-					break;
-				default:
-					if (format[i] == '\0')
-						exit(0);
-					_putchar('%');
-					_putchar(format[i]);
-					break;
-			}
-			i++;
-			continue;
+			switch_statement(format, i, num, ap);
 		}
-		_putchar(format[i]);
+		else
+			_putchar(format[i]);
 		i++;
 	}
 	num += i;
