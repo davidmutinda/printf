47010116b198fb9bf7d45a67b3594fd9dc54ba4b
Gideon Osoro <89097948+Gideon-O@users.noreply.github.com>
2022-04-20 23:26:01 +0300

    Update main.h for print_numbers
    Header file updated, function to be written

--- main.h
+++ main.h
@@ -1,7 +1,10 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 #include <stdarg.h>
+
 int _printf(const char *format, ...);
 int _putchar(char c);
 int switch_statement(const char *format, int i, va_list ap);
+void print_numbers(char *); /* function to print numbers, to be written */
+
 #endif
