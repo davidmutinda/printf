a4e5d9c8dc9e1fba58cd3f1850776a719f24a1d9
Gideon-O <osorogideon10@gmail.com>
2022-04-20 12:50:06 -0700

    %d and %i

--- switch.c
+++ switch.c
@@ -49,10 +49,8 @@ int switch_statement(const char *format, int i, va_list ap)
 
 		case 'i':
 			integer = va_arg(ap, int);
-			if (decimal != NULL)
-				print_string(integer);
-			num = strlen(integer) - 1;
 			break;
+
 		case '%':
 			_putchar('%');
 			num = -1;
