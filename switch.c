8a25c5a2c77da18542eebd4145fc411737e5c30d
David Kyalo <davidkymutinda@gmail.com>
2022-04-20 04:57:36 -0700

    Create switch.c

---
+++ switch.c
@@ -0,0 +1,61 @@
+#include <stdarg.h>
+#include <string.h>
+#include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
+/**
+ * print_string - prints string
+ * @p: string
+ * Return: void
+ */
+void print_string(char *p)
+{
+	int n;
+
+	n = 0;
+	while (p[n] != '\0')
+	{
+		_putchar(p[n]);
+		n++;
+	}
+}
+/**
+ * switch_statement - entry point
+ * @format: string
+ * @i: integer for iteration
+ * @num: total number of characters printed
+ * @ap: argument pointer
+ * Return: void
+ */
+void switch_statement(const char *format, int i, int num, va_list ap)
+{
+	char *p;
+	char charc;
+
+	switch (format[i])
+	{
+		case 'c':
+			charc = va_arg(ap, int);
+			num -= 1;
+			_putchar(charc);
+			break;
+		case 's':
+			p = va_arg(ap, char*);
+			if (p == NULL)
+				p = "(null)";
+			num = strlen(p) - 2;
+			print_string(p);
+			break;
+		case '%':
+			_putchar('%');
+			num -= 1;
+			break;
+		default:
+			if (format[i] == '\0')
+				exit(0);
+			_putchar('%');
+			_putchar(format[i]);
+			break;
+	}
+
+}
